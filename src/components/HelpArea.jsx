937e88110122381f2e93662ac70c35ef25efa97e
GiorgiGabelia <giorgi.gabelia321@gmail.com>
2025-05-23 16:12:23 +0400

    modify paragraph

--- src/components/HelpArea.jsx
+++ src/components/HelpArea.jsx
@@ -22,7 +22,7 @@ const HELP_ITEMS = [
 function HelpArea() {
   return (
     <section data-testid="help-area" id="help-area">
-      <p>TEST</p>
+      <p>TEST 22222</p>
       {HELP_ITEMS.map((item) => (
         <HelpBox key={item.id} title={item.title} text={item.text} />
       ))}
